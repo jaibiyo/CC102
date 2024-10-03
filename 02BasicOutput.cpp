3a1467039cf54bbfffd81773e4c2efe1b3c1489c
jaibiyo <jasperacunin7@gmail.com>
2024-10-03 10:11:28 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+	cout << " OUR LADY OF LOURDES COLLEGE FOUNDATION" << endl;
+	cout << " CCS " << endl;
+	cout << " BSIT 1 A " << endl;
+	cout << " JASPER S ACUNIN " << endl;
+	cout << " P-5 Brgy Lag-on Daet Camarines Norte" << endl;
+	
+return 0;
+
+}
