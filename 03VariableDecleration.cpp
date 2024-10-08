0b7cb924db8fb321fe0793d7d90c225d0c095441
jaibiyo <jasperacunin7@gmail.com>
2024-10-08 09:44:21 +0800

    Add files via upload

---
+++ 03VariableDecleration.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+	string Age;
+
+	cout << "Enter Your Age\t:";
+	cin >> Age;
+	cout << "_____________________________________________" << endl;
+	cout << " Your Age is " << Age << endl;
+	cout << " ____________________________________________" << endl;
+	cout << " Thank You! ";
+	
+	
+return 0;
+
+}
